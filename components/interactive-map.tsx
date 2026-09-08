fca0565df4a5a17c8cad3f900af18c0f52767a87
tathana <thanaphatchainarong@gmail.com>
2026-09-09 00:23:09 +0700

    Fix Leaflet map tile URL typo and add invalidateSize

--- components/interactive-map.tsx
+++ components/interactive-map.tsx
@@ -84,7 +84,7 @@ export default function InteractiveMap({
 
       const tileUrl = layerType === 'satellite'
         ? 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-        : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{js}';
+        : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
       L.tileLayer(tileUrl, {
         attribution: '&copy; OpenStreetMap / Esri',
@@ -124,6 +124,9 @@ export default function InteractiveMap({
       });
 
       map.flyTo([activeStation.lat, activeStation.lng], 12, { duration: 1.2 });
+      setTimeout(() => {
+        try { map.invalidateSize(); } catch {}
+      }, 300);
     };
 
     if (!script) {
